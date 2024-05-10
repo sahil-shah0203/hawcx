f3d3179c6398c91875fb21e310d2da6da44e87e2
Peter <peter.dyumin@gmail.com>
2024-05-11 01:34:22 +0300

    Add HTTPS, add basic layout

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 ## js-biometrics
 
+Browser biometric authentication demo
+
+### Launch
+
+```shell script
+npm run dev
+```
+
+Server will be available at https://localhost:5173
+
 ### License
 
 [MIT](./LICENSE.md)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import ssl from '@vitejs/plugin-basic-ssl'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ssl()
+  ]
 })
